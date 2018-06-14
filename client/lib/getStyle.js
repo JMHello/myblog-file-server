518f1d0bbf35fe3cdfebce7c4f1d752b0744f7b5
jm <471938302@qq.com>
2018-06-14 23:57:41 +0800

    prod

--- client/lib/getStyle.js
+++
@@ -1,7 +0,0 @@
-export function getComputedStyle (ele, cName) {
-  if (window.getComputedStyle) { // 非ie
-    return window.getComputedStyle(ele, null)[cName]
-  } else {
-    return ele.currentStyle[cName] // ie
-  }
-}
--- test/controller/address.test.js
+++
@@ -1,9 +0,0 @@
-import getAddress from '../../server/controllers/address'
-
-describe('test trie tree', () => {
-  test('get 82 provinces', async () => {
-    const root = getAddress()
-    
-    expect(root.edges.length).toBe(83)
-  })
-})
