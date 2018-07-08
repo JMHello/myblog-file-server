830079d1b6e5aa76555f67e77d1007dc540441f0
jm <471938302@qq.com>
2018-07-08 16:38:05 +0800

    prod

---
+++ ecosystem.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      // 应用进程的名称
+      name      : 'blogfile',
+      // 启动脚本路径
+      script    : './server/index.js',
+      watch: [
+        'dist'
+      ],
+      ignore_watch: [
+        'node_modules',
+        'upload'
+      ],
+      env: {
+        COMMON_VARIABLE: 'true',
+        NODE_ENV: 'production'
+      },
+      error_file: './logs/app-err.log',
+      out_file: './logs/app-out.log'
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  // deploy : {
+  //   production : {
+  //     user : 'node',
+  //     host : '212.83.163.1',
+  //     ref  : 'origin/master',
+  //     repo : 'git@github.com:repo.git',
+  //     path : '/var/www/production',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  //   },
+  //   dev : {
+  //     user : 'node',
+  //     host : '212.83.163.1',
+  //     ref  : 'origin/master',
+  //     repo : 'git@github.com:repo.git',
+  //     path : '/var/www/development',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+  //     env  : {
+  //       NODE_ENV: 'dev'
+  //     }
+  //   }
+  // }
+};
